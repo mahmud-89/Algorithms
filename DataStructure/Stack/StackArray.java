e04484af15b6589741a610d79aa9a90dab90935f
= <mahmudur.rahman.csedu@gmail.com>
2020-11-19 20:48:38 +0600

    StackArray updated

--- DataStructure/Stack/StackArray.java
+++ DataStructure/Stack/StackArray.java
@@ -12,26 +12,34 @@ package DataStructure.Stack;
 public class StackArray {
 
     private int size;
-    private int[] stackArray;
+    private int[] stack;
     private final int DEFAULT_VALUE;
     private int MaxSize;
+    private int top;
 
-    private int top = -1;
-
+    
+    
     public StackArray() {
-        this.size = 0;
-        this.DEFAULT_VALUE = 5;
-        this.MaxSize = this.DEFAULT_VALUE;
-        this.stackArray = new int[DEFAULT_VALUE];
+        this(5);
     }
+    
+    public StackArray(int size){
+        this.size = 0;
+        this.DEFAULT_VALUE = size;
+        this.MaxSize = this.DEFAULT_VALUE;
+        this.stack = new int[DEFAULT_VALUE];
+        this.top = -1;
+    }
+    
+    
 
     public void resizeArray(int newSize) {
         this.MaxSize = newSize;
         int temp[] = new int[this.MaxSize];
         for (int i = 0; i < this.size(); i++) {
-            temp[i] = stackArray[i];
+            temp[i] = stack[i];
         }
-        this.stackArray = temp;
+        this.stack = temp;
     }
 
     public void push(int value) {
@@ -39,7 +47,7 @@ public class StackArray {
             resizeArray(MaxSize * 2);
             System.out.println("size increased! cur size : " + this.MaxSize);
         }
-        this.stackArray[++top] = value;
+        this.stack[++top] = value;
         ++this.size;
     }
 
@@ -53,8 +61,8 @@ public class StackArray {
             System.out.println("size decreased! cur size: " + this.MaxSize);
         }
 
-        int data = stackArray[top];
-        stackArray[top] = 0;
+        int data = stack[top];
+        stack[top] = 0;
         --top;
         --size;
         return data;
@@ -64,11 +72,18 @@ public class StackArray {
         if (isEmpty()) {
             throw new NullPointerException("element does not exist in the list");
         }
-        return this.stackArray[top];
+        return this.stack[top];
     }
 
     public boolean isEmpty() {
         return this.size == 0;
+    }
+    
+    public void clear(){
+        this.MaxSize = this.DEFAULT_VALUE;
+        this.size = 0;
+        this.stack = new int[this.DEFAULT_VALUE];
+        this.top = -1;
     }
 
     public int size() {
@@ -83,15 +98,15 @@ public class StackArray {
         }
         int temp = top;
         while(temp>=0){
-            sb.append(this.stackArray[temp]).append("->");
+            sb.append(this.stack[temp]).append("->");
             temp--;
         }
         return sb.replace(sb.length()-2, sb.length(), "").toString();
     }
     
-    public void displayStackArray(){
+    public void displayStackElements(){
         for(int i =0 ;i<this.MaxSize;i++){
-            System.out.print(this.stackArray[i] + " ");
+            System.out.print(this.stack[i] + " ");
         }
         System.out.println("");
     }
@@ -115,8 +130,14 @@ public class StackArray {
         System.out.println("Pop: " + sad.pop());
         System.out.println("Pop: " + sad.pop());
         
-        sad.displayStackArray();
+        System.out.println("showing list elements...");
+        sad.displayStackElements();
         System.out.println("size of the list: " + sad.size());
+        
+        System.out.println("clearing list..");
+        sad.clear();
+        sad.displayStackElements();
+        System.out.println(sad.size());
 
     }
 }
