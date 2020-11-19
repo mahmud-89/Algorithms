8c3ddb08dc2c4394d4fa94e385e3c065dc1d006a
= <mahmudur.rahman.csedu@gmail.com>
2020-11-19 07:57:32 +0600

    Dynamic array Edited

--- DataStructure/Lists/DynamicArray.java
+++ DataStructure/Lists/DynamicArray.java
@@ -17,88 +17,103 @@ import practice.DynamicArrayDemo;
  * @author promise
  * @param <E>
  */
-public class DynamicArray<E> implements Iterable<E>{
+public class DynamicArray<E> implements Iterable<E> {
 
     private int capacity;
     private int size;
     private Object[] elements;
-    
-    public DynamicArray(final int capacity){
+
+    public DynamicArray(final int capacity) {
         this.size = 0;
         this.capacity = capacity;
         this.elements = new Object[this.capacity];
     }
-    public DynamicArray(){
+
+    public DynamicArray() {
         this.size = 0;
         this.capacity = 10;
         this.elements = new Object[this.capacity];
-    
+
     }
-    
-    public int newCapacity(){
-        this.capacity  *= 2;
+
+    public DynamicArray(E[] array) {
+        this.size = 0;
+        this.capacity = 10;
+        this.elements = new Object[capacity];
+        addArray(array);
+
+    }
+
+    public int newCapacity() {
+        this.capacity *= 2;
         return this.capacity;
     }
-    
-    public void add(final E element){
-        if(this.size == this.elements.length){
+
+    public void add(final E element) {
+        if (this.size == this.elements.length) {
             this.elements = Arrays.copyOf(this.elements, newCapacity());
         }
-        
+
         this.elements[this.size] = element;
         size++;
     }
-   
-    public void put(final int index,final E element){
+
+    public void put(final int index, final E element) {
         this.elements[index] = element;
     }
-    
-    public E get(final int index){
+
+    public E get(final int index) {
         return getElement(index);
     }
-    
-    public int getSize(){
+
+    public int getSize() {
         return this.size;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return this.size == 0;
     }
-    
-    public E remove (final int index){
+
+    public E remove(final int index) {
         final E oldElement = getElement(index);
         fastRemove(elements, index);
-        
+
         return oldElement;
     }
-    
-    private void fastRemove(final Object[] elements,final int index){
+
+    private void fastRemove(final Object[] elements, final int index) {
         final int newSize = this.size - 1;
-        
-        if(index>=getSize()){
+
+        if (index >= getSize()) {
             throw new NoSuchElementException("no element found at this position");
         }
-        
-        if(newSize>index){
-            System.arraycopy(elements, index+1, elements, index, newSize - index);
-        }   
-        
+
+        if (newSize > index) {
+            System.arraycopy(elements, index + 1, elements, index, newSize - index);
+        }
+
         elements[this.size = newSize] = null;
     }
-    
-    private E getElement(final int index){
+
+    private E getElement(final int index) {
         return (E) this.elements[index];
     }
-    
+
+    public void addArray(E[] array) {
+        for (int i = 0; i < array.length; i++) {
+            add(array[i]);
+        }
+    }
+
     @Override
     public Iterator<E> iterator() {
-       return new DynamicArrayIterator();
+        return new DynamicArrayIterator();
     }
-    
+
     private class DynamicArrayIterator implements Iterator<E> {
 
         private int cursor;
-        
+
         @Override
         public boolean hasNext() {
             return this.cursor != size;
@@ -106,12 +121,14 @@ public class DynamicArray<E> implements Iterable<E>{
 
         @Override
         public E next() {
-            if(this.cursor >= DynamicArray.this.size) 
+            if (this.cursor >= DynamicArray.this.size) {
                 throw new NoSuchElementException();
-            
-            if(this.cursor >= DynamicArray.this.elements.length)
+            }
+
+            if (this.cursor >= DynamicArray.this.elements.length) {
                 throw new ConcurrentModificationException();
-        
+            }
+
             final E element = DynamicArray.this.getElement(this.cursor);
             this.cursor++;
             return element;
@@ -119,26 +136,40 @@ public class DynamicArray<E> implements Iterable<E>{
 
         @Override
         public void remove() {
-            if(this.cursor<0) throw new IllegalStateException();
+            if (this.cursor < 0) {
+                throw new IllegalStateException();
+            }
             DynamicArray.this.remove(this.cursor);
             this.cursor--;
         }
-        
+
     }
-    
-    
-    public static void main(String[] s){
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < getSize(); i++) {
+            sb.append(elements[i]).append(",");
+        }
+        return sb.replace(sb.length() - 1, sb.length(), "]").toString();
+    }
+
+    public static void main(String[] s) {
         DynamicArray<String> names = new DynamicArray<>();
         names.add("mahmudur");
         names.add("rahman");
         names.add("promise");
 
         Iterator<String> itr = names.iterator();
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             System.out.println(itr.next());
         }
-        
-        
+        System.out.println("");
+        String arr[] = {"a", "b", "c"};
+        names.addArray(arr);
+
+        System.out.println(names.toString());
     }
-    
+
 }
