77ecdde5039ce7d2c3f61295bbf45a3d60f76b7b
= <mahmudur.rahman.csedu@gmail.com>
2020-12-06 08:28:06 +0600

    Graph implemented file uploaded

---
+++ DataStructure/Graph/Graph.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataStructure.Graph;
+
+/**
+ *
+ * @author promise
+ */
+import DataStructure.Queues.GenericQueue;
+import DataStructure.Stacks.StackArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class Graph {
+
+    Map<Integer, ArrayList<Integer>> map;
+
+    public Graph() {
+        map = new HashMap<>();
+    }
+
+    public Graph(int capacity) {
+        map = new HashMap<>(capacity);
+    }
+
+    public void addEdge(int source, int destination, boolean bidirectional) {
+        if (!map.containsKey(source)) {
+            this.map.put(source, new ArrayList<>());
+            this.map.get(source).add(destination);
+        } else {
+            this.map.get(source).add(destination);
+        }
+
+        if (!map.containsKey(destination)) {
+            this.map.put(destination, new ArrayList<>());
+        }
+
+        if (bidirectional) {
+            this.map.get(destination).add(source);
+        }
+    }
+
+    public boolean isExist(int val) {
+        return this.map.containsKey(val);
+    }
+
+    public boolean isConnected(int u, int v) {
+        if (map.containsKey(u)) {
+            return map.get(u).contains(v);
+        }
+        return false;
+    }
+
+    public void printGraph() {
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
+            Integer vertex = entry.getKey();
+            System.out.printf("vertex %d is connected with: ", vertex);
+            for (Integer val : map.get(vertex)) {
+                System.out.print(val + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public int sizeOfVertices() {
+        return this.map.keySet().size();
+    }
+
+    public void traverseBFS(int startingVertex) {
+        GenericQueue<Integer> queue = new GenericQueue<>();
+        boolean visited[] = new boolean[sizeOfVertices()];
+        queue.enqueue(startingVertex);
+        visited[startingVertex] = true;
+        System.out.printf("bfs traversing from (%d): ", startingVertex);
+        while (!queue.isEmpty()) {
+            Integer val = queue.dequeue();
+            System.out.print(val + " ");
+            for (Integer v : this.map.get(val)) {
+                if (!visited[v]) {
+                    queue.enqueue(v);
+                    visited[v] = true;
+                }
+            }
+        }
+        System.out.println("");
+    }
+
+    //iterative
+    public void traverseDFS(int startingVertex){
+        boolean discoverd[] = new boolean[sizeOfVertices()];
+        StackArrayList<Integer> stack = new StackArrayList<>();
+        stack.push(startingVertex);
+        discoverd[startingVertex] = true;
+        System.out.printf("DFS traversing from vertex (%d): ",startingVertex);
+        while(!stack.isEmpty()){
+            Integer val = stack.pop();
+            System.out.print(val + " ");
+            for(Iterator<Integer> itr = this.map.get(val).listIterator();itr.hasNext();){
+                int n = itr.next();
+                if(!discoverd[n]){
+                    stack.push(n);
+                    discoverd[n] = true;
+                }
+            }
+        }
+        System.out.println("");
+    }
+    
+    //recursive
+    public void traverseRecursiveDFS(int startingVertex,boolean [] discoverd){
+        System.out.print(startingVertex+ " ");
+        discoverd[startingVertex] = true;
+        for(Integer val : this.map.get(startingVertex)){
+            if(!discoverd[val]){
+                discoverd[val] = true;
+                traverseRecursiveDFS(val, discoverd);
+            }
+        }
+    }
+ 
+   
+
+}
+
+class Runner {
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        boolean bidirectional = false;
+        g.addEdge(1, 0, bidirectional);
+        g.addEdge(0, 2, bidirectional);
+        g.addEdge(2, 1, bidirectional);
+        g.addEdge(0, 3, bidirectional);
+        g.addEdge(1, 4, bidirectional);
+        g.printGraph();
+        System.out.println("size of vertices: " + g.sizeOfVertices());
+        g.traverseBFS(1);
+        g.traverseDFS(0);
+        boolean discoverd[] = new boolean[g.sizeOfVertices()];
+        int startingVertex = 0;
+        System.out.printf("DFS traversing from %d: ",startingVertex);
+        g.traverseRecursiveDFS(startingVertex,discoverd);
+
+    }
+}
+
+/**
+ *      g.addEdge(1, 2, bidirectional);
+        g.addEdge(1, 4, bidirectional);
+        g.addEdge(1, 7, bidirectional);
+        g.addEdge(2, 5, bidirectional);
+        g.addEdge(2, 6, bidirectional);
+        g.addEdge(5, 7, bidirectional);
+        g.addEdge(6, 4, bidirectional);
+        g.addEdge(6, 3, bidirectional);
+        g.addEdge(3, 8, bidirectional);
+ */
