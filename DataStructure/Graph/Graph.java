92f5d09049b3a2896bfc8d109ef74c6a2c63a218
= <mahmudur.rahman.csedu@gmail.com>
2020-12-07 11:57:30 +0600

    graph impl updated

--- DataStructure/Graph/Graph.java
+++ DataStructure/Graph/Graph.java
@@ -14,6 +14,7 @@ import DataStructure.Stack.StackArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
@@ -57,58 +58,103 @@ public class Graph {
         }
         return false;
     }
-    
-    public boolean hasCycle(){
+
+    public boolean hasCycle(boolean bidirectional) {
         boolean visited[] = new boolean[sizeOfVertices()];
-        boolean recStack[] = new boolean[sizeOfVertices()];
-        for(int i = 0;i<sizeOfVertices();i++){
-            if(hasCycleUtil(i, visited, recStack)){
-                System.out.println("cycle found when starting v is: "+i);
-                return true;
+        if (bidirectional) {
+
+        } else {
+            boolean recStack[] = new boolean[sizeOfVertices()];
+            for (int i = 0; i < sizeOfVertices(); i++) {
+                if (hasCycleDirectedUtil(i, visited, recStack)) {
+                    System.out.println("cycle found when starting v is: " + i);
+                    return true;
+                }
+                System.out.println("rejected :" + i);
             }
-            System.out.println("rejected: "+i);
         }
         return false;
     }
-    
-    public boolean hasCycleUtil(int v,boolean visited[],boolean recStack[]){
-        if(recStack[v]){
-            //for(boolean b: recStack) System.out.println("value of recStack: "+b);
-            System.out.println("recStack base: "+v);
+
+    public boolean hasCycleDirectedUtil(int v, boolean visited[], boolean recStack[]) {
+        if (recStack[v]) {
+            System.out.println("recStack base: " + v);
             return true;
         }
-        if(visited[v]){
-            System.out.println("visited base: "+v);
+        if (visited[v]) {
+            System.out.println("visited base: " + v);
             return false;
         }
         recStack[v] = true;
         visited[v] = true;
-        for(int val : this.map.get(v)){
-            if(hasCycleUtil(val, visited, recStack)){
+
+        List<Integer> children = this.map.get(v);
+
+        for (int val : children) {
+            if (hasCycleDirectedUtil(val, visited, recStack)) {
                 return true;
             }
         }
-        System.out.println("outer loop: "+ v);
+        System.out.println("outer loop: " + v);
         recStack[v] = false;
+        return false;
+    }
+
+    //approach-1(bfs)
+    public boolean hasCycleUndirectedUtilBFS(int v) {
+        boolean visited[] = new boolean[sizeOfVertices()];
+        int flag[] = new int[sizeOfVertices()];
+        GenericQueue<Integer> queue = new GenericQueue<>();
+
+        for (int i = 0; i < flag.length; i++) {
+            flag[i] = -1;
+        }
+        queue.enqueue(v);
+        flag[v] = 0;
+        while (!queue.isEmpty()) {
+            Integer data = queue.dequeue();
+            visited[data] = true;
+            flag[data] = 1;
+            for (Integer val : this.map.get(data)) {
+                if (flag[val] == 0) {
+                    System.out.println("resulting value:" + val);
+                    return true;
+                }
+                if (!visited[val]) {
+                    queue.enqueue(val);
+                    flag[val] = 0;
+                }
+            }
+        }
+
+        return false;
+    }
+    //approach-2(Dfs)
+    public boolean hasCycleUndirectedUtilDFS(int v, boolean visited,int parent){
+        
         return false;
     }
 
     public int sizeOfVertices() {
         return this.map.keySet().size();
     }
-    
-    public int sizeOfEdges(boolean bidirectional){
+
+    public int sizeOfEdges(boolean bidirectional) {
         int counter = 0;
-        for(int i = 0;i<sizeOfVertices();i++){
-            counter+=this.map.get(i).size();
+        for (int i = 0; i < sizeOfVertices(); i++) {
+            counter += this.map.get(i).size();
         }
-        if(bidirectional){
-            return counter/2;
+        if (bidirectional) {
+            return counter / 2;
         }
-        
+
         return counter;
     }
-    
+
+    public void clearGraph() {
+        this.map.clear();
+    }
+
     public void printGraph() {
         for (Map.Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
             Integer vertex = entry.getKey();
@@ -119,7 +165,6 @@ public class Graph {
             System.out.println("");
         }
     }
-
 
     public void traverseBFS(int startingVertex) {
         GenericQueue<Integer> queue = new GenericQueue<>();
@@ -141,18 +186,18 @@ public class Graph {
     }
 
     //iterative
-    public void traverseDFS(int startingVertex){
+    public void traverseDFS(int startingVertex) {
         boolean discoverd[] = new boolean[sizeOfVertices()];
         StackArrayList<Integer> stack = new StackArrayList<>();
         stack.push(startingVertex);
         discoverd[startingVertex] = true;
-        System.out.printf("DFS traversing from vertex (%d): ",startingVertex);
-        while(!stack.isEmpty()){
+        System.out.printf("DFS traversing from vertex (%d): ", startingVertex);
+        while (!stack.isEmpty()) {
             Integer val = stack.pop();
             System.out.print(val + " ");
-            for(Iterator<Integer> itr = this.map.get(val).listIterator();itr.hasNext();){
+            for (Iterator<Integer> itr = this.map.get(val).listIterator(); itr.hasNext();) {
                 int n = itr.next();
-                if(!discoverd[n]){
+                if (!discoverd[n]) {
                     stack.push(n);
                     discoverd[n] = true;
                 }
@@ -160,13 +205,13 @@ public class Graph {
         }
         System.out.println("");
     }
-    
+
     //recursive
-    public void traverseRecursiveDFS(int startingVertex,boolean [] discoverd){
-        System.out.print(startingVertex+ " ");
+    public void traverseRecursiveDFS(int startingVertex, boolean[] discoverd) {
+        System.out.print(startingVertex + " ");
         discoverd[startingVertex] = true;
-        for(Integer val : this.map.get(startingVertex)){
-            if(!discoverd[val]){
+        for (Integer val : this.map.get(startingVertex)) {
+            if (!discoverd[val]) {
                 traverseRecursiveDFS(val, discoverd);
             }
         }
@@ -183,40 +228,49 @@ class Runner {
         g.addEdge(1, 4, bidirectional);
         g.addEdge(4, 3, bidirectional);
         g.addEdge(3, 1, bidirectional);
-        
+        //g.addEdge(3, 4, bidirectional);
+
 //        g.addEdge(2, 0, bidirectional);
 //        g.addEdge(2, 3, bidirectional);
 //        g.addEdge(3, 3, bidirectional);
-        
         g.printGraph();
-        System.out.println("size of vertices: " + g.sizeOfVertices());
-        
+
         g.traverseBFS(1);
         g.traverseDFS(0);
-        
+
         //recursive dfs
         boolean discoverd[] = new boolean[g.sizeOfVertices()];
         int startingVertex = 0;
-        System.out.printf("DFS traversing from %d: ",startingVertex);
-        g.traverseRecursiveDFS(startingVertex,discoverd);
+        System.out.printf("DFS traversing from %d: ", startingVertex);
+        g.traverseRecursiveDFS(startingVertex, discoverd);
         System.out.println("");
-        
+
+        System.out.println("size of vertices: " + g.sizeOfVertices());
         System.out.println("number of edges: " + g.sizeOfEdges(bidirectional));
         System.out.println("\n\nchecking cycle...");
-        System.out.println("has cycle? : "+g.hasCycle());
-        
+        System.out.println("has cycle? : " + g.hasCycle(bidirectional));
+
+        System.out.println("\n\nclearing graph...");
+        g.clearGraph();
+
+        bidirectional = true;
+        g.addEdge(0, 1, bidirectional);
+        g.addEdge(0, 2, bidirectional);
+        g.addEdge(2, 5, bidirectional);
+        g.addEdge(1, 4, bidirectional);
+        g.addEdge(1, 3, bidirectional);
+        //g.addEdge(3, 4, bidirectional);
        
+
+        System.out.println("has cycle?" + g.hasCycleUndirectedUtilBFS(0));
+
     }
 }
 
 /**
- *      g.addEdge(1, 2, bidirectional);
-        g.addEdge(1, 4, bidirectional);
-        g.addEdge(1, 7, bidirectional);
-        g.addEdge(2, 5, bidirectional);
-        g.addEdge(2, 6, bidirectional);
-        g.addEdge(5, 7, bidirectional);
-        g.addEdge(6, 4, bidirectional);
-        g.addEdge(6, 3, bidirectional);
-        g.addEdge(3, 8, bidirectional);
+ * g.addEdge(1, 2, bidirectional); g.addEdge(1, 4, bidirectional); g.addEdge(1,
+ * 7, bidirectional); g.addEdge(2, 5, bidirectional); g.addEdge(2, 6,
+ * bidirectional); g.addEdge(5, 7, bidirectional); g.addEdge(6, 4,
+ * bidirectional); g.addEdge(6, 3, bidirectional); g.addEdge(3, 8,
+ * bidirectional);
  */
