f2857c92c0f8917fadad3be8c40b6c70710e4317
= <mahmudur.rahman.csedu@gmail.com>
2020-12-24 16:59:06 +0600

    minHeap implemented

--- sorting_algorithm/heap_datastructure/using_array/MinHeapArray.java
+++ sorting_algorithm/heap_datastructure/using_array/MinHeapArray.java
@@ -8,9 +8,19 @@ package sorting_algorithm.heap_datastructure.using_array;
 import java.util.Arrays;
 
 /**
- *
+ * In this solution we counting Heap index from 1. 
+ * But if index from 0 then properties will be: 
+ *  parent : (i-1)/2
+ *  leftChild : (2*i) + 1
+ *  rightChild: (2*i) + 2
+ *  leaf nodes range: (n/2) to n (indexing from 0)
+ *                     and ((n/2) +1 to n (indexing from 1) 
+ *                     here, n is the number of nodes/elements in the array.
+ *  non-leaf: index:0 --> 0 to (n/2)+1
+ *            index:1 --> 1 to (n/2)
  * @author promise
  */
+
 public class MinHeapArray {
     private int[] Heap;
     private int size;
@@ -48,38 +58,53 @@ public class MinHeapArray {
         }
     }
     
-    private boolean isLeaf(int pos){
-        if(pos >= (size/2) +1 && pos <= size){
-            System.out.println("pos :"+pos +" size: "+size);
-            System.out.println(Heap[pos]);
-            return true;
-        }
-        return false;
-    }
-    
+    //second approach 
     public void minHeapify(int pos){
-        System.out.println(Heap[pos]);
-        if(isLeaf(pos)){
-            return;
+        int left = leftChild(pos);
+        int right = rightChild(pos);
+        int smallest;
+        if(left <= size && Heap[left] < Heap[pos]){
+            smallest = left;
+        }else smallest = pos;
+        
+        if(right <= size && Heap[right] < Heap[smallest]){
+            smallest = right;
         }
-        if(Heap[pos] > Heap[leftChild(pos)]
-                || Heap[pos] > Heap[rightChild(pos)]){
-            if(Heap[leftChild(pos)] < Heap[rightChild(pos)]){
-                swap(pos, leftChild(pos));
-                minHeapify(leftChild(pos));
-            }else{
-                swap(rightChild(pos), pos);
-                minHeapify(rightChild(pos));
-            }
+        
+        if(smallest != pos){
+            swap(smallest, pos);
+            minHeapify(smallest);
         }
     }
-    
+   
     public int extractMin(){
         int popped = this.Heap[1];
         swap(1, size);
         size--;
-        minHeapify(1);
+        //minHeapify(1);
         return popped;
+    }
+    
+    public void print(){
+        for(int i = 1;i<= size/2;i++){
+            if(leftChild(i) <= size && rightChild(i) <= size){
+                System.out.print(" PARENT : " + Heap[i] + " LEFT CHILD : "
+                    + Heap[2 * i] + " RIGHT CHILD :" + Heap[2 * i + 1]);
+            }else if(leftChild(i) <= size){
+                
+                System.out.print(" PARENT : " + Heap[i] + " LEFT CHILD : "
+                    + Heap[2 * i]);
+            }
+            System.out.println("");
+        }
+    }
+    
+    // Function to remove and return the minimum 
+    // element from the heap 
+    public void minHeap(){
+        for(int pos = size/2;pos>=1;pos--){
+            minHeapify(pos);
+        }
     }
     
     public int [] getHeap(){
@@ -88,22 +113,22 @@ public class MinHeapArray {
     
     public static void main(String[] args) {
         MinHeapArray minHeap = new MinHeapArray(15);
-//        minHeap.insert(5); 
-//        minHeap.insert(3); 
-//        minHeap.insert(17); 
-//        minHeap.insert(10); 
-//        minHeap.insert(84); 
-//        minHeap.insert(19); 
-//        minHeap.insert(6); 
-//        minHeap.insert(22); 
-//        minHeap.insert(9);
+        minHeap.insert(5); 
+        minHeap.insert(3); 
+        minHeap.insert(17); 
+        minHeap.insert(10); 
+        minHeap.insert(84); 
+        minHeap.insert(19); 
+        minHeap.insert(6); 
+        minHeap.insert(22); 
+        minHeap.insert(9);
 
-        minHeap.insert(10);
-        minHeap.insert(20);
-        minHeap.insert(30);
+        minHeap.print();
         
         int min = minHeap.extractMin();
         System.out.println("minimum value is : "+min);
+        minHeap.minHeap();
+        minHeap.print();
        
         System.out.println(Arrays.toString(minHeap.getHeap()));
     }
